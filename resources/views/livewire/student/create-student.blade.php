1ac80afe426ea1d0b2977d4d689036f0326dc78b
Pirashanth <161277817+mydynamica07@users.noreply.github.com>
2025-02-14 22:23:12 +0530

    update create student

---
+++ resources/views/livewire/student/create-student.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <form action="" method="post"></form>
+    <lable for="firstName">First Name:</lable>
+    <input type="text" id="firstName" wire:model="firstName"><br><br>
+
+    <lable for="lastName">Last Name:</lable>
+    <input type="text" id="lastName" wire:model="lastName"><br><br>
+
+    <button wire:click="save"> Save  </button><br><br>
+
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Counter;
+use App\Livewire\Student\CreateStudent;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/counter', Counter::class);
+Route::get('/create-student', CreateStudent::class);
